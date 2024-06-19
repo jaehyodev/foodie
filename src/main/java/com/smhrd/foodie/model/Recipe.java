b47f25d1fe68658051a126905dc9de17d0d04b4c
hyoj <kyshs45@naver.com>
2024-06-19 13:58:31 +0900

    효정커밋

---
+++ src/main/java/com/smhrd/foodie/model/Recipe.java
@@ -0,0 +1,21 @@
+package com.smhrd.foodie.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recipe {
+	   private int recipe_idx;
+	   private String recipe_name;
+	   private String recipe_cat;
+	   private String recipe_content;
+	   private int recipe_time;
+	   private int recipe_portion;
+	   private String recipe_all_ingre;
+	   private String recipe_cook;
+	   private String recipe_title_img;
+	   private int recipe_views_cnt;
+}
