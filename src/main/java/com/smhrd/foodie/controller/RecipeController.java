d4f9894147c93f0783a8e9721cd57cd8841938b2
hyoj <kyshs45@naver.com>
2024-06-19 17:19:20 +0900

    Recipe 페이지 수정

--- src/main/java/com/smhrd/foodie/controller/RecipeController.java
+++ src/main/java/com/smhrd/foodie/controller/RecipeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,9 +20,12 @@ public class RecipeController {
 	RecipeMapper mapper;
 	
 	@RequestMapping(value="/recipe/{recipe_cat}", method=RequestMethod.GET)
-	public void recipecat(@PathVariable("recipe_cat") String recipe_cat) {
-		List<Recipe> list = mapper.select(recipe_cat);
+	public String recipecat(@PathVariable("recipe_cat") String recipe_cat, Model model) {
+		List<Recipe> recipeList = mapper.select(recipe_cat);
+		model.addAttribute("recipeList", recipeList);
 		
-		System.out.println(list.get(0).getRecipe_name());
+		return "recipe";
 	}
+	
+	
 }
