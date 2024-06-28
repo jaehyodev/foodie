2a7eb00316c7ac72c297de75bea4a1e537fe577a
Jaehyo <kimjaehyo0623@gmail.com>
2024-06-28 09:42:07 +0900

    jaehyo

--- src/main/java/com/smhrd/foodie/controller/MemberRestController.java
+++ src/main/java/com/smhrd/foodie/controller/MemberRestController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.smhrd.foodie.mapper.MemberMapper;
 import com.smhrd.foodie.model.Member;
