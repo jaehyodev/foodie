02c3b68dcbbacbf16d715689ad61bb021a048d19
Jaehyo <kimjaehyo0623@gmail.com>
2024-07-03 17:51:11 +0900

    comment

--- src/main/java/com/smhrd/foodie/model/ChatCompletionResponse.java
+++ src/main/java/com/smhrd/foodie/model/ChatCompletionResponse.java
@@ -6,11 +6,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * ChatCompletionResponse 클래스는 ChatGPT API와 같은 인공지능 기반 챗봇 시스템에서 사용자의 입력에 대한 응답을 구조화하는 데 사용.
+ * choices 필드를 통해 여러 개의 선택지를 가진 리스트로 사용자에게 제공할 수 있는 다양한 응답 옵션을 저장.
+ */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatCompletionResponse {
 	
+	// AI가 보여줄 답변 리스트 (index, message로 구성)
 	private List<Choice> choices;
 	
 	@Data
--- src/main/java/com/smhrd/foodie/model/ChatMessage.java
+++ src/main/java/com/smhrd/foodie/model/ChatMessage.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+
+// AI와 USER가 주고받는 메시지
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatMessage {
 
-	private String role;
-	private String content;
+	private String role;		// 역할 (user, assistant로 사용 중)
+	private String content;	// 메시지 내용
+	
 }
