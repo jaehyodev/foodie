05de912003097f990e12c95c16eaf3fe7dfd156b
Jaehyo <kimjaehyo0623@gmail.com>
2024-12-06 20:25:49 +0900

    security: config.properties 삭제, root-context.xml 수정 및 application.properties 추가로 보안 설정

--- src/main/java/com/smhrd/foodie/config/AppConfig.java
+++ src/main/java/com/smhrd/foodie/config/AppConfig.java
@@ -1,29 +1,24 @@
 package com.smhrd.foodie.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@PropertySource("classpath:config.properties") // config.properties 파일에서 환경 설정 가져오기.
-@ComponentScan(basePackages = "com.smhrd.foodie") // com.smhrd.foodie 패키지에서 컴포넌트를 스캔하여 빈으로 등록.
+@ComponentScan(basePackages = "com.smhrd.foodie")
 public class AppConfig {
 
-	// 환경 설정
-	// API Key가 노출된 상태로 GitHub에 업로드 되지 않음.
-	// config.properties에서 API Key 숨길 수 있도록 해줌.
+	@Value("${chatgpt.api-key}")
+    private String chatgptApiKey;
 	
 	// RestTemplate 빈을 생성.
 	@Bean
 	public RestTemplate restTemplate(Environment env) {
 		
 		RestTemplate restTemplate = new RestTemplate();
-		
-		// config.properties 파일에서 ChatGPT_API_KEY 값을 가져오기.
-		String chatgptApiKey = env.getProperty("ChatGPT_API_KEY");
 		
 		// RestTemplate 인터셉터를 추가하여 HTTP 요청 헤더에 Authorization 헤더를 추가.
 		restTemplate.getInterceptors().add((request, body, execution) -> {
